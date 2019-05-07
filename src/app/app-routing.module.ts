2c8e0ac9e97448235eb2971fae41688c82535798
Thomas Tighe <ttighe@reisystems.com>
2019-05-07 15:58:29 -0400

    paths and route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -69,7 +69,7 @@ const routes: Routes = [
   {
     path: 'search',
 
-    loadChildren: './sam-ui-patterns/sam-search/sam-search.module#SamSearchModule',
+    loadChildren: './search/sam-search.module#SamSearchModule',
     data: {
       id: "search"
     }
