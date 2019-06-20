3f24b0c886d2a6728616e9bcc71aa03f6dd15a7a
Thomas Tighe <ttighe@reisystems.com>
2019-06-20 15:29:57 -0400

    fal side menu

--- src/app/workspace/workspace-routing.module.ts
+++ src/app/workspace/workspace-routing.module.ts
@@ -24,22 +24,19 @@ const routes: Routes = [
 				path: 'contractopportunities',
 				loadChildren: './opportunities/opportunities.module#OpportunitiesModule',
 				data: {
-					sideId: ''
+					sideId: 'contractopportunities'
 				}
 			},
 			{
 				path: 'contractdata',
 				loadChildren: './contractdata/contractdata.module#ContractdataModule',
 				data: {
-					sideId: ''
+					sideId: 'contractdata'
 				}
 			},
 			{
 				path: 'fal',
-				loadChildren: './assistance/assistance.module#AssistanceModule',
-				data: {
-					sideId: ''
-				}
+				loadChildren: './assistance/assistance.module#AssistanceModule'
 			},
 			{
 				path: 'registrations',
@@ -52,14 +49,14 @@ const routes: Routes = [
 				path: 'exclusions',
 				loadChildren: './exclusions/exclusions.module#ExclusionsModule',
 				data: {
-					sideId: ''
+					sideId: 'exclusions'
 				}
 			},
 			{
 				path: 'integrity',
 				loadChildren: './integrity/integrity.module#IntegrityModule',
 				data: {
-					sideId: ''
+					sideId: 'integrityinfo'
 				}
 
 			},
@@ -67,7 +64,7 @@ const routes: Routes = [
 				path: 'profile',
 				loadChildren: './profile/profile.module#ProfileModule',
 				data: {
-					sideId: ''
+					sideId: 'profile'
 				}
 			},
 			{
@@ -81,14 +78,14 @@ const routes: Routes = [
 				path: 'reset',
 				loadChildren: './reset-password/reset-password.module#ResetPasswordModule',
 				data: {
-					sideId: ''
+					sideId: 'reset'
 				}
 			},
 			{
 				path: 'roles',
 				loadChildren: './user-roles/user-roles.module#UserRolesModule',
 				data: {
-					sideId: ''
+					sideId: 'roles'
 				}
 			},
 			{
