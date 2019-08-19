31cda4ef80d265944cbe85f1bad6e983796dc93e
Christy Hermansen <christy.hermansen@gsa.gov>
2019-08-19 11:36:47 -0400

    Fixed build issue.  Local build, even with --aot flag, still is missing things caught by master build.

--- src/app/services/user/user.module.ts
+++ src/app/services/user/user.module.ts
@@ -1,21 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UserProfile } from './user-profile';
-import { Role } from './role';
-import { RoleAssignment } from './role-assignment';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    UserProfile,
-    Role,
-    RoleAssignment
   ],
   exports: [
-  	UserProfile,
-  	Role,
-  	RoleAssignment
   ]
 })
 export class UserModule { }
