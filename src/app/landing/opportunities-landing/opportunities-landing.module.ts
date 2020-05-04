b0cd64cbdc83ce211324f04a8e7660535d379d2d
Christy Hermansen <christy.hermansen@gsa.gov>
2020-05-03 23:20:08 -0400

    Fixed build error

--- src/app/landing/opportunities-landing/opportunities-landing.module.ts
+++ src/app/landing/opportunities-landing/opportunities-landing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import {
   SdsSearchModule
@@ -13,6 +14,7 @@ import { OpportunitiesLandingComponent } from './opportunities-landing.component
   imports: [
     CommonModule,
     SdsSearchModule,
+    RouterModule,
     OpportunitiesLandingRoutingModule
   ],
   exports: [OpportunitiesLandingComponent]
