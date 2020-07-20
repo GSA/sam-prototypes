e73e2f575d6ea0381bc397d35d518a71f85366b7
Samantha Baker <38532587+samanthabaker@users.noreply.github.com>
2020-07-20 15:54:58 -0400

    federal hierarchy page

---
+++ src/app/landing/federal-hierarchy-landing/federal-hierarchy-landing.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-federal-hierarchy-landing',
+  templateUrl: './federal-hierarchy-landing.component.html',
+  styleUrls: ['./federal-hierarchy-landing.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class FederalHierarchyLandingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  search(term) {
+
+  }
+
+}
