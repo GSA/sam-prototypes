a01e9a74c3fade0607d2ca7f0ed028bc4217f1da
Christy Hermansen <christy.hermansen@gsa.gov>
2021-03-20 10:17:53 -0400

    Continued breaking home page into components

---
+++ src/app/home/home-register-tile/home-register-tile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'home-register-tile',
+  templateUrl: './home-register-tile.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HomeRegisterTileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
