863f815ea024df4821820a71c7daa582e044ce9a
Cameron Wolf <cameron.wolf@gsa.gov>
2021-04-13 10:50:43 -0400

    Adding ability to specify template to override default

--- src/app/landing/shared/landing-feed/landing-feed.component.ts
+++ src/app/landing/shared/landing-feed/landing-feed.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy, TemplateRef } from '@angular/core';
 import { NavigationMode } from '@gsa-sam/components';
 import { KnowledgeService, KnowledgeArticle } from '../../../services/knowledge-services/knowledge.service';
 
@@ -6,23 +6,27 @@ import { KnowledgeService, KnowledgeArticle } from '../../../services/knowledge-
   selector: 'landing-feed',
   template: `
     <div class="sds-feed">
-      <div *ngFor="let item of model" class="event margin-0">
-        <navigate [item]="item">
-	        <div class="content">
-	          <div class="summary">
-	            <div class="date">{{item.publishDate | date:item.dateFormat }}</div>
-	            <a class="action">{{item.text}}</a>
-	          </div>
-	          <div class="meta">
-	            <em>{{item.displayDescription}}</em>
-	          </div>
-	        </div>
-	    </navigate>
-      </div>
+      <ng-container [ngTemplateOutlet]="template || default" [ngTemplateOutletContext]="{ $implicit: model}"></ng-container>
     </div>
     <div class="padding-2 text-right more-news-link">
         <a class="usa-link" [href]="url">More Announcements</a>
     </div>
+
+    <ng-template #default let-item>
+      <div *ngFor="let item of model" class="event margin-0">
+        <navigate [item]="item">
+          <div class="content">
+            <div class="summary">
+              <div class="date">{{item.publishDate | date:item.dateFormat }}</div>
+              <a class="action">{{item.text}}</a>
+            </div>
+            <div class="meta">
+              <em>{{item.displayDescription}}</em>
+            </div>
+          </div>
+        </navigate>
+        </div>
+    </ng-template>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
@@ -32,7 +36,10 @@ export class LandingFeedComponent implements OnInit {
   public model: KnowledgeArticle[];
   public url: string;
 
-  constructor() { 
+  @Input()
+  template: TemplateRef<any>
+
+  constructor() {
   }
 
   ngOnInit(): void {
