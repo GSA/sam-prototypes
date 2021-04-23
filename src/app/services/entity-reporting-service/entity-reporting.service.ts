b6839f42ad3d68c8a63a7f58244a88fa32068ef0
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2021-04-23 15:04:57 -0400

    service changes

--- src/app/services/entity-reporting-service/entity-reporting.service.ts
+++ src/app/services/entity-reporting-service/entity-reporting.service.ts
@@ -1,24 +1,31 @@
-import { Injectable } from '@angular/core';
-import { SearchParameters } from '@gsa-sam/layouts';
-import { AbstractSearchableService } from '../abstract-searchable-service/abstract-searchable.service';
+import { Injectable } from "@angular/core";
+import { SearchParameters } from "@gsa-sam/layouts";
+import { AbstractSearchableService } from "../abstract-searchable-service/abstract-searchable.service";
 
-import { EntityReportingServiceModule } from './entity-reporting-service.module';
+import { EntityReportingServiceModule } from "./entity-reporting-service.module";
 
-import { contracts } from './awards.data';
+import { contracts } from "./awards.data";
 
 @Injectable({
-  providedIn: EntityReportingServiceModule
+  providedIn: EntityReportingServiceModule,
 })
 export class EntityReportingService extends AbstractSearchableService {
-
   private data: any[] = contracts;
 
-  constructor() { 
-  	super();
+  constructor() {
+    super();
   }
 
   getSortedFilteredData(search: SearchParameters): any[] {
-      return this.data;
+    return this.data;
   }
 
+  getFilteredDataById(id) {
+    console.log(this.data);
+    let filteredData = this.data.find(
+      (item) => item.vendor.globalDunsNumber === id
+    );
+    console.log(filteredData);
+    return filteredData;
+  }
 }
